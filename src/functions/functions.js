3397abaa135f95fe38ce9f9ebb040eb8d5719c14
origin <lkov@post.cz>
2019-01-27 17:04:42 +0100

    prettified

--- src/functions/functions.js
+++ src/functions/functions.js
@@ -2,4 +2,5 @@ import placeShip from "./placeShip";
 import BlankBoard from "./blankboard";
 import Ship from "./ship";
 import rotateGrid from "./rotategrid";
+
 export { placeShip, BlankBoard, Ship, rotateGrid };
