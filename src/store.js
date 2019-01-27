9f3474df6e9617f2904012ef82dd4e0e4a7e115b
origin <lkov@post.cz>
2019-01-27 16:58:46 +0100

    import redux-thunk middleware

--- src/store.js
+++ src/store.js
@@ -1,8 +1,8 @@
 import { createStore, applyMiddleware } from "redux";
-// import thunk from 'redux-thunk';
+import thunk from "redux-thunk";
 import rootReducers from "./reducers";
 
-const middleware = [];
+const middleware = [thunk];
 
 const initialState = {};
 
