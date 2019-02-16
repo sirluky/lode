c2646006de9c6974aa356e69b935e54c1fd5ebeb
origin <lkov@post.cz>
2019-02-17 00:13:31 +0100

    changed to redux wrapper

--- src/App.js
+++ src/App.js
@@ -1,25 +1,20 @@
 import React, { Component } from "react";
 import "./App.css";
+
 import MyBoard from "./components/MyBoard";
 import Visualization from "./components/Visualization";
 import ControlPanel from "./components/ControlPanel";
 import { Provider } from "react-redux";
 import store from "./store";
 import ShipSelect from "./components/ShipSelect";
+import Main from "./main";
+import { connect } from "react-redux";
+// import { placeBoat } from "../actions/gameActions";
 class App extends Component {
   render() {
     return (
       <Provider store={store}>
-        <div className="App">
-          <div className="container">
-            <Visualization />
-            <ControlPanel />
-            <div className="grid">
-              <MyBoard />
-              <ShipSelect />
-            </div>
-          </div>
-        </div>
+        <Main />
       </Provider>
     );
   }
