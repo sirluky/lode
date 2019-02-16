9cb792c0eda7b3361a2790e4dfdce1e2931b1f65
origin <lkov@post.cz>
2019-02-17 00:17:10 +0100

    on press start send to server ships data

--- src/components/ControlPanel.js
+++ src/components/ControlPanel.js
@@ -1,15 +1,25 @@
 import React, { Component } from "react";
 import "./controlpanel.scss";
-export default class ControlPanel extends Component {
+import { connect } from "react-redux";
+import { playerReady } from "../actions/gameActions";
+
+class ControlPanel extends Component {
   render() {
     return (
       <div className="controlPanel">
         <div className="wrapper">
           <div className="mytime time">00:00</div>
-          <div className="exit time">Ukončit</div>
+          <div className="exit time" onClick={e => this.props.playerReady()}>
+            Ukončit
+          </div>
           <div className="opponentTime time">00:00</div>
         </div>
       </div>
     );
   }
 }
+const mapStateToProps = state => ({});
+export default connect(
+  mapStateToProps,
+  { playerReady }
+)(ControlPanel);
