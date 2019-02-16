0cef6e11a20848039aae27fbe4c383e437d922f5
origin <lkov@post.cz>
2019-02-17 00:20:28 +0100

    global socket io across components and redux

---
+++ src/functions/socket.js
@@ -0,0 +1,17 @@
+import openSocket from "socket.io-client";
+const socket = openSocket("http://localhost:80");
+// function subscribeToTimer(cb) {
+//   socket.on("timer", timestamp => cb(null, timestamp));
+//   socket.emit("subscribeToTimer", 1000);
+// }
+function on(type, akce) {
+  socket.on(type, function(d) {
+    akce(d);
+  });
+}
+
+let emit = (type, data) => {
+  socket.emit(type, data);
+  console.log(socket);
+};
+export { emit, on };
