b4e20e17ab8c2e3f4ceccb4e7f2a450399b6faf0
origin <lkov@post.cz>
2019-02-18 19:49:34 +0100

    Now using lodash + new shoot and hit reducer function

--- src/reducers/gameReducer.js
+++ src/reducers/gameReducer.js
@@ -2,12 +2,14 @@ import {
   CHANGE_STATUS,
   PLACE_BOAT,
   SELECT_BOAT,
-  PLAYER_READY
+  PLAYER_READY,
+  SHOOT,
+  HIT
 } from "../actions/types";
 import { BlankBoard } from "../functions/functions";
 import { Ship } from "../functions/functions";
 // import io from "socket.io-client";
-
+import _ from "lodash";
 import { emit } from "../functions/socket";
 //statuses
 
@@ -21,6 +23,7 @@ function initial() {
 }
 const initialState = {
   board: initial(),
+  enemyboard: initial(),
   ships: {
     selected: "none",
     rotation: 1,
@@ -33,6 +36,27 @@ const initialState = {
 
 export default function(state = initialState, action) {
   switch (action.type) {
+    case SHOOT:
+      const enboard = state.enemyboard;
+      if (action.hit) enboard[action.position].type = "shiphit";
+      else {
+        enboard[action.position].type = "missed";
+      }
+      return {
+        ...state,
+        enemyboard: [...enboard]
+      };
+    case HIT:
+      const board = state.board;
+      if (action.hit) board[action.position].type = "shiphit";
+      else {
+        board[action.position].type = "missed";
+      }
+
+      return {
+        ...state,
+        board: [...board]
+      };
     case PLAYER_READY:
       // console.log(state.ships.placed);
       emit(PLAYER_READY, { ships: state.ships.placed });
@@ -47,24 +71,40 @@ export default function(state = initialState, action) {
       // console.log(state.ships.rotation);
       let boat;
       if (state.ships.selected !== "none") {
-        boat = new Ship(state.ships.selected, action.pos, state.cid);
-        boat.setRotation(state.ships.rotation);
-        nPlaced = [...state.ships.placed, boat];
-        updated = boat.setPos(action.pos, copied);
-        // console.log("new", updated);
+        // console.log(state.ships.offers[0].type, state.ships.selected);
+        const selectedship = state.ships.selected;
+        let shipindex = _.findIndex(
+          state.ships.offers,
+          ship => ship.type === selectedship
+        );
+        // alert(shipindex);
+        let theship = state.ships.offers[shipindex];
+        if (theship.remaining > 0) {
+          boat = new Ship(state.ships.selected, action.pos, state.cid);
+          boat.setRotation(state.ships.rotation);
+          nPlaced = [...state.ships.placed, boat];
+          updated = boat.setPos(action.pos, copied);
+          // console.log("new", updated);
+
+          state.ships.offers[shipindex].remaining = theship.remaining - 1;
+          return {
+            ...state,
+            cid: state.cid + 1,
+            board: updated,
+            ships: {
+              ...state.ships,
+              offers: [...state.ships.offers],
+              placed: nPlaced
+            }
+          };
+        }
       } else {
         nPlaced = [...state.ships.placed];
         updated = copied;
+        return state;
       }
-      return {
-        ...state,
-        cid: state.cid + 1,
-        board: updated,
-        ships: {
-          ...state.ships,
-          placed: nPlaced
-        }
-      };
+      return state;
+
     case SELECT_BOAT:
       let rotation = state.ships.rotation;
       if (state.ships.selected === action.payload.type)
@@ -78,8 +118,10 @@ export default function(state = initialState, action) {
         }
       };
     case CHANGE_STATUS:
+      let enemyboard = action.enemydata.enemyboard;
       return {
         ...state,
+        enemyboard,
         status: action.status
       };
 
