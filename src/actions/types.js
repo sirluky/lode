f3a8ec3e6122e17f465d79e72faf5baf6f91360d
origin <lkov@post.cz>
2019-02-18 19:42:42 +0100

    hit and shoot types

--- src/actions/types.js
+++ src/actions/types.js
@@ -3,3 +3,6 @@ export const PLACE_BOAT = "PLACE_BOAT";
 export const PLAYER_READY = "PLAYER_READY";
 export const SELECT_BOAT = "SELECT_BOAT";
 export const CHANGE_STATUS = "CHANGE_STATUS";
+
+export const SHOOT = "SHOOT";
+export const HIT = "HIT";
