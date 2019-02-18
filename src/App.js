6f81d33418e789d6161280ade3af2b93ea135459
origin <lkov@post.cz>
2019-02-18 19:40:39 +0100

    deleted old unused stuffs

--- src/App.js
+++ src/App.js
@@ -1,14 +1,14 @@
 import React, { Component } from "react";
 import "./App.css";
 
-import MyBoard from "./components/MyBoard";
-import Visualization from "./components/Visualization";
-import ControlPanel from "./components/ControlPanel";
+// import MyBoard from "./components/MyBoard";
+// import Visualization from "./components/Visualization";
+// import ControlPanel from "./components/ControlPanel";
 import { Provider } from "react-redux";
 import store from "./store";
-import ShipSelect from "./components/ShipSelect";
+// import ShipSelect from "./components/ShipSelect";
 import Main from "./main";
-import { connect } from "react-redux";
+// import { connect } from "react-redux";
 // import { placeBoat } from "../actions/gameActions";
 class App extends Component {
   render() {
