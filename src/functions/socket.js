af6b8557169629b94b7fe0fbd8ee6633bc1ce9f1
origin <lkov@post.cz>
2019-02-18 19:46:15 +0100

    changed default port

--- src/functions/socket.js
+++ src/functions/socket.js
@@ -1,5 +1,5 @@
 import openSocket from "socket.io-client";
-const socket = openSocket("http://localhost:80");
+const socket = openSocket("http://localhost:9378");
 // function subscribeToTimer(cb) {
 //   socket.on("timer", timestamp => cb(null, timestamp));
 //   socket.emit("subscribeToTimer", 1000);
@@ -12,6 +12,7 @@ function on(type, akce) {
 
 let emit = (type, data) => {
   socket.emit(type, data);
-  console.log(socket);
+  // console.log(socket);
 };
+
 export { emit, on };
