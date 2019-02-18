9c8b11db1b00865a11bde0f707a54562fe673f5e
origin <lkov@post.cz>
2019-02-18 19:41:48 +0100

    hit and shoot actions

--- src/actions/gameActions.js
+++ src/actions/gameActions.js
@@ -2,5 +2,7 @@ import placeBoat from "./placeBoat";
 import selectBoat from "./selectBoat";
 import playerReady from "./PlayerReady";
 import changeStatus from "./changeStatus";
+import hit from "./hit";
+import shoot from "./shoot";
 
-export { placeBoat, selectBoat, playerReady, changeStatus };
+export { placeBoat, selectBoat, playerReady, changeStatus, hit, shoot };
