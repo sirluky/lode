82b66a65b35c713ab7102f67e30cb139e399ac2d
origin <lkov@post.cz>
2019-02-18 19:47:58 +0100

    enemy board component

---
+++ src/ingame-components/enemy.js
@@ -0,0 +1,46 @@
+import React, { Component } from "react";
+import "./boards.scss";
+import { connect } from "react-redux";
+// import { emit, on } from "../functions/socket";
+import { shoot } from "../actions/gameActions";
+class BattleBoard extends Component {
+  componentDidMount = () => {};
+
+  cellClicked(e) {
+    // console.log(e.target.getAttribute("shot"));
+    if (e.target.getAttribute("shot"))
+      this.props.shoot(parseInt(e.target.getAttribute("index")));
+  }
+  // componentReady = () => {
+  //   console.log(this.props);
+  //   // emit();
+
+  // };
+
+  render() {
+    return (
+      <div className="board enemy attackMode">
+        {this.props.board.map((e, index) => (
+          <div
+            className={"cell " + e.type}
+            key={index}
+            shot={e.type === "blank" ? "true" : "false"}
+            index={index}
+            onClick={this.cellClicked.bind(this)}
+          >
+             
+          </div>
+        ))}
+      </div>
+    );
+  }
+}
+
+const mapStateToProps = state => ({
+  board: state.game.enemyboard
+});
+
+export default connect(
+  mapStateToProps,
+  { shoot }
+)(BattleBoard);
