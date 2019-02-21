3216bfb68d0306f4c19d943116a020092e282bc7
origin <lkov@post.cz>
2019-02-21 21:47:49 +0100

    text changing in parts of game

--- src/components/ControlPanel.js
+++ src/components/ControlPanel.js
@@ -2,25 +2,35 @@ import React, { Component } from "react";
 import "./controlpanel.scss";
 import { connect } from "react-redux";
 import { playerReady, changeStatus } from "../actions/gameActions";
-
+import { JOIN_GAME, CHANGE_STATUS } from "../actions/types";
 class ControlPanel extends Component {
   render() {
     return (
       <div className="controlPanel">
         <div className="wrapper">
-          {console.log(this.props)}
+          {/* {this.props.changeStatus(CHANGE_STATUS, {
+            nick: this.props.mynick
+          })} */}
           <div className="mytime time">{this.props.mynick}</div>
           <div
             className="exit time"
             onClick={e => {
               this.props.playerReady();
-              this.props.changeStatus("waiting");
+              if (e.currentTarget.innerText === "Spustit hru")
+                this.props.changeStatus(CHANGE_STATUS, {
+                  nick: this.props.mynick
+                });
+              else this.props.changeStatus("waiting");
             }}
             style={{
               visibility: this.props.status !== "ingame" ? "visible" : "hidden"
             }}
           >
-            {this.props.status === "lobby" ? "Spustit hru" : "Připraven"}
+            {this.props.status === "lobby"
+              ? "Spustit hru"
+              : this.props.status === "waiting"
+              ? "Čekání na soupeře"
+              : "Připraven"}
           </div>
           <div className="opponentTime time">{this.props.ennick}</div>
         </div>
