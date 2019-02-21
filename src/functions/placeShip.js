c33f8b29dd6a68d18830e7d6cf33497f7e838508
origin <lkov@post.cz>
2019-02-21 21:50:10 +0100

    checking collisions, left side is  still buggy

--- src/functions/placeShip.js
+++ src/functions/placeShip.js
@@ -1,5 +1,5 @@
 // import { rotateGrid } from "../functions/functions";
-
+import cutBoard from "./cutBoard";
 /**
  *
  * @param {*} arr pole na ktere chcete umistit objekt [{type: "blank", id:"0"}], bude nahrazeno na danych pozicich {type:"lod", id: -mu bude prideleno podle zadanych parametru}
@@ -10,45 +10,38 @@
  * @param {*} rotation rotace pole 1 - nic, 2 - 90deg, 3 - 180deg, 4 - 270deg
  *
  */
-export default function placeShip(arr, size = 10, ship) {
-  // console.log(ship);
-  // let toDelete = [];
-  // let success = false;
-  // console.log(ship.pos);
-  // if (ship.pos % size > 0 && ship.pos % size < size - 1) {
-  // success = true
-  // if (ship.prev.pos !== null) {
-  //   let prevship = rotateGrid(ship.dmodel, ship.prev.rotation);
-  //   prevship.forEach((row, oy) =>
-  //     row.forEach((cell, ox) => {
-  //       if (cell === 1) {
-  //         let cellPos = ship.prev.pos + oy * size + ox - 11;
-  //         // const arrcell = arr[cellPos];
-  //         // if (arrcell.id == ship.id) {
-  //         toDelete.push(cellPos);
-  //         // }
-  //       }
-  //     })
-  //   );
-  // }
+export default function placeShip(arr, size = 10, ship, placeable = false) {
+  if (placeable === true) {
+    ship.cmodel.forEach((row, ty) =>
+      row.forEach((modelcell, tx) => {
+        if (modelcell !== 0) {
+          let cpos = ty * size + ship.pos + tx;
+          cpos -= size - 1;
+          console.log(cpos + size);
 
+          if (
+            checkAround(
+              [
+                arr[cpos + 1],
+                arr[cpos - 1],
+                arr[cpos - size],
+                arr[cpos + size]
+              ],
+              arr[cpos]
+            )
+          )
+            placeable = false;
+        }
+      })
+    );
+
+    return placeable;
+  }
   ship.cmodel.forEach((row, ty) =>
     row.forEach((modelcell, tx) => {
       if (modelcell !== 0) {
         let cpos = ty * size + ship.pos + tx;
         cpos -= size - 1;
-
-        // let toSplice = null;
-
-        // toDelete.forEach((e, index) => {
-        //   if (e === cpos) {
-        //     toSplice = index;
-        //   }
-        // });
-
-        // if (toSplice !== null) {
-        //   toDelete.splice(toSplice, 1);
-        // }
         arr[cpos] = { id: ship.id, type: "lod" };
       }
     })
@@ -59,4 +52,19 @@ export default function placeShip(arr, size = 10, ship) {
   // const last = arr[ty * size + ship.pos + tx];
 
   return arr;
+}
+export function checkAround(checkplaces, currentplace) {
+  // console.log(checkplaces, "check");
+
+  for (let i = 0; i < checkplaces.length; i++) {
+    // if (checkplaces[i] === undefined) return false;
+
+    if (checkplaces[i].type === "lod") {
+      console.log(checkplaces[i], "z", i);
+      return true;
+    }
+  }
+  if (currentplace.type === "nic") {
+    return true;
+  }
 }
