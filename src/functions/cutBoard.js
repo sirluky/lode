3bfc337c95a7296ecd15776af75e5845bffb84e1
origin <lkov@post.cz>
2019-02-21 21:49:33 +0100

    cut board function + test

---
+++ src/functions/cutBoard.js
@@ -0,0 +1,65 @@
+import _ from "lodash";
+/**
+ * @param arr pole ze ktereho se bude vyrezavat
+ * @param pos pozice (index)
+ *
+ */
+export default function cutBoard(
+  arr,
+  dsize = 10,
+  pos,
+  csize = 5,
+  placeholderval,
+  mode = false
+) {
+  let pomtwod = [];
+  let twoD = [];
+  _.forEach(arr, e => {
+    pomtwod.push(e);
+    if (pomtwod.length === dsize) {
+      twoD.push(pomtwod);
+      pomtwod = [];
+    }
+  });
+  // twoD.push(pomtwod);
+
+  // console.log(twoD);
+  let cutarr = [];
+
+  let ypos = Math.floor(pos / dsize);
+  let xpos = pos % dsize;
+
+  let xuntil = xpos + csize;
+  let yuntil = ypos + csize;
+  // if (mode) {
+  //   let pomarr = [];
+  //   while (yuntil > ypos) {
+  //     while (xuntil > xpos) {
+  //       if (xpos >= dsize || ypos >= dsize || xpos < 0 || ypos < 0) {
+  //         pomarr.push(8);
+  //       } else {
+  //         pomarr.push(twoD[ypos][xpos]);
+  //       }
+  //       xpos++;
+  //     }
+  //     cutarr.push(pomarr);
+  //     pomarr = [];
+  //     xpos -= csize;
+  //     ypos++;
+  //   }
+  // } else {
+  while (yuntil > ypos) {
+    while (xuntil > xpos) {
+      if (xpos >= dsize || ypos >= dsize || xpos < 0 || ypos < 0) {
+        cutarr.push(placeholderval);
+      } else {
+        cutarr.push(twoD[ypos][xpos]);
+      }
+      xpos++;
+    }
+    xpos -= csize;
+    ypos++;
+    // }
+  }
+  return cutarr;
+}
