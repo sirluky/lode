a72b648ec799bfe04b706d13cf4360cc2a5b2b5c
origin <lkov@post.cz>
2019-02-24 19:13:31 +0100

    new types of boats

--- src/assets/index.js
+++ src/assets/index.js
@@ -1,5 +1,7 @@
 import mediumImg from "./images/lod-m.svg";
 import smallImg from "./images/lod-s.svg";
+import microImg from "./images/lod-micro.svg";
+import quadImg from "./images/lod-quad.svg";
 
 /**
  *
@@ -11,5 +13,7 @@ import smallImg from "./images/lod-s.svg";
 export const ShipTypes = {
   none: { img: "", model: [[0]] },
   small: { img: smallImg, model: [[0, 0, 0], [0, 1, 0], [0, 1, 0]] },
-  medium: { img: mediumImg, model: [[0, 1, 0], [0, 1, 1], [0, 1, 0]] }
+  medium: { img: mediumImg, model: [[0, 1, 0], [0, 1, 1], [0, 1, 0]] },
+  quad: { img: quadImg, model: [[0, 1, 0], [1, 1, 1], [0, 1, 0]] },
+  micro: { img: microImg, model: [[0, 0, 0], [0, 1, 0], [0, 0, 0]] }
 };
