1ccb2f87abd533d680252e808f4b8b0847250fd6
Lukáš Kovář <38957065+sirluky@users.noreply.github.com>
2024-05-12 13:54:22 +0200

    chore: update api url

--- src/functions/socket.js
+++ src/functions/socket.js
@@ -1,27 +1,17 @@
 import openSocket from "socket.io-client";
-const socket = openSocket('http://localhost:8080' /*"https://lode-server.herokuapp.com/" */);
-// const socket = openSocket("https://8080-bakespinelessgopher.cdr.co/");
-console.log(process.env.NODE_ENV)
 
-// function subscribeToTimer(cb) {
-//   socket.on("timer", timestamp => cb(null, timestamp));
-//   socket.emit("subscribeToTimer", 1000);
-// }
-function on(type, akce) {
+const socket = openSocket(process.env.REACT_APP_API_URL);
+
+function on(type, action) {
   socket.on(type, function (d) {
-    akce(d);
+    action(d);
   });
 }
 
-let emit = (type, data) => {
+const emit = (type, data) => {
   socket.emit(type, data);
-  // console.log(socket);
 };
-/**
- *
- * @param {*} type typ eventu
- *
- */
+
 function onOnce(type) {
   return new Promise(resolve => {
     socket.once(type, function (data) {
